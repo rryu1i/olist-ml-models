441a7b01821f47206bede85a658466dfa8be1ea3
roger.issonaga@gmail.com <roger.issonaga@gmail.com>
2023-04-16 18:10:39 +0000

    Atualizado com pip install tqdm

--- src/etl/ingestao_feature_store.py
+++ src/etl/ingestao_feature_store.py
@@ -1,47 +1,50 @@
 # Databricks notebook source
 # MAGIC %pip install tqdm
-# MAGIC 
-# MAGIC import datetime
-# MAGIC from tqdm import tqdm
-# MAGIC 
-# MAGIC def import_query(path):
-# MAGIC     with open(path, 'r') as open_file:
-# MAGIC         return open_file.read()
-# MAGIC 
-# MAGIC 
-# MAGIC def table_exists(database, table):
-# MAGIC     count = (spark.sql(f"SHOW TABLES FROM {database}")
-# MAGIC                   .filter(f"tableName = '{table}'")
-# MAGIC                   .count())
-# MAGIC     return count > 0
-# MAGIC 
-# MAGIC 
-# MAGIC def date_range(dt_start, dt_stop, period='daily'):
-# MAGIC     datetime_start = datetime.datetime.strptime(dt_start, '%Y-%m-%d')
-# MAGIC     datetime_stop = datetime.datetime.strptime(dt_stop, '%Y-%m-%d')
-# MAGIC     dates = []
-# MAGIC     while datetime_start <= datetime_stop:
-# MAGIC         dates.append(datetime_start.strftime("%Y-%m-%d"))
-# MAGIC         datetime_start += datetime.timedelta(days=1)
-# MAGIC     if period == 'daily':
-# MAGIC         return dates
-# MAGIC     elif period == 'monthly':
-# MAGIC         return [i for i in dates if i.endswith("01")]
-# MAGIC 
-# MAGIC table = dbutils.widgets.get("table")
-# MAGIC table_name = f"fs_vendedor_{table}"
-# MAGIC database = "silver.analytics"
-# MAGIC period = dbutils.widgets.get("period")
-# MAGIC 
-# MAGIC query = import_query(f"{table}.sql")
-# MAGIC 
-# MAGIC date_start = dbutils.widgets.get("date_start")
-# MAGIC date_stop = dbutils.widgets.get("date_stop")
-# MAGIC 
-# MAGIC dates = date_range(date_start, date_stop, period)
-# MAGIC 
-# MAGIC print(table_name, table_exists(database, table_name))
-# MAGIC print(date_start, ' ~ ', date_stop)
+
+# COMMAND ----------
+
+
+import datetime
+from tqdm import tqdm
+
+def import_query(path):
+    with open(path, 'r') as open_file:
+        return open_file.read()
+
+
+def table_exists(database, table):
+    count = (spark.sql(f"SHOW TABLES FROM {database}")
+                  .filter(f"tableName = '{table}'")
+                  .count())
+    return count > 0
+
+
+def date_range(dt_start, dt_stop, period='daily'):
+    datetime_start = datetime.datetime.strptime(dt_start, '%Y-%m-%d')
+    datetime_stop = datetime.datetime.strptime(dt_stop, '%Y-%m-%d')
+    dates = []
+    while datetime_start <= datetime_stop:
+        dates.append(datetime_start.strftime("%Y-%m-%d"))
+        datetime_start += datetime.timedelta(days=1)
+    if period == 'daily':
+        return dates
+    elif period == 'monthly':
+        return [i for i in dates if i.endswith("01")]
+
+table = dbutils.widgets.get("table")
+table_name = f"fs_vendedor_{table}"
+database = "silver.analytics"
+period = dbutils.widgets.get("period")
+
+query = import_query(f"{table}.sql")
+
+date_start = dbutils.widgets.get("date_start")
+date_stop = dbutils.widgets.get("date_stop")
+
+dates = date_range(date_start, date_stop, period)
+
+print(table_name, table_exists(database, table_name))
+print(date_start, ' ~ ', date_stop)
 
 # COMMAND ----------
 
